bfdd74dae68b032325cd55d27b8f5b23d0806cb4
kil <godngu@gmail.com>
2021-11-28 09:32:20 +0900

    ThreadLocal

--- src/main/java/learn/spring/advanced/LogTraceConfig.java
+++ src/main/java/learn/spring/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package learn.spring.advanced;
 
-import learn.spring.advanced.trace.logtrace.FieldLogTrace;
 import learn.spring.advanced.trace.logtrace.LogTrace;
+import learn.spring.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
